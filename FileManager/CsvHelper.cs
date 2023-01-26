8e09668a416030ba067cad90a5b030f10e8649da
CHAUVEAU Nathan <Nathan.CHAUVEAU@cpnv.ch>
2023-01-26 16:21:17 +0100

    Rendu

--- FileManager/CsvHelper.cs
+++ FileManager/CsvHelper.cs
@@ -4,12 +4,22 @@
     {
         #region private attributs
         //TODO Private attributs - 2pts
+        private string _path = "";
+        private string _fileName = "";
+        private char _seperator;
         #endregion private attributs
 
         #region constructor
         public CsvHelper(string path, string fileName, char separator = ';') : base(path, fileName)
         {
             //TODO Constructor - 3pts
+            _path = path;
+            _fileName = fileName;
+            if (IsCharSupported(separator))
+            {
+                throw new UnsupportedSeparatorException();
+            }
+            _seperator = separator;
         }
         #endregion constructor
 
@@ -17,6 +27,25 @@
         public void ExtractFileContent()
         {
             //TODO ExtractFileContent - 6pts
+            StreamReader streamReader = new StreamReader(_fullPath);
+            string line;
+            // Reads and stores lines from the file until eof.
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                this.Lines.Add(line);
+
+            }
+            streamReader.Close();
+
+            if ( this.Lines.Contains(";;"))
+            {
+                
+                throw new InOutLib.StructureException();
+            }
+            if(this.Lines.Contains(""))
+            {
+                throw new StructureException();
+            } 
         }
         #endregion public methods
 
@@ -24,7 +53,11 @@
         private bool IsCharSupported(char separator)
         {
             //TODO IsCharSupported - 2pts
-            throw new NotImplementedException();
+            if (separator != ';')
+            {
+                return true;
+            }
+            return false;
         }
         #endregion privates methods
 
